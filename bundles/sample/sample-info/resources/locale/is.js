89c63dad5da2bd4667bc7056aa034e3b8f0fcd51
Sami Mäkinen <sami.makinen@nls.fi>
2021-10-18 16:09:00 +0300

    ESLint auto fixes

--- bundles/sample/sample-info/resources/locale/is.js
+++ bundles/sample/sample-info/resources/locale/is.js
@@ -1,12 +1,11 @@
 Oskari.registerLocalization(
     {
-        "lang": "is",
-        "key": "sample-info",
-        "value": {
-            "guidedTour": {
-                "title": "Innskráning",
-                "message": "Þetta er Landupplýsingagátt (Oskari). Ekki þarf notendanafn til að nota hana, en ef þú hefur fengið úthlutað notendanafn þá er hægt að innskrá sig með því."
+        lang: 'is',
+        key: 'sample-info',
+        value: {
+            guidedTour: {
+                title: 'Innskráning',
+                message: 'Þetta er Landupplýsingagátt (Oskari). Ekki þarf notendanafn til að nota hana, en ef þú hefur fengið úthlutað notendanafn þá er hægt að innskrá sig með því.'
             }
         }
     });
-    
