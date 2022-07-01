424ccff3b41976abdfd2f9ab3df362dc76bf002f
Aho Henri <vk585@maanmittauslaitos.fi>
2022-07-01 13:25:24 +0300

    Add metadataflyout bundle.

--- applications/embedded/main.js
+++ applications/embedded/main.js
@@ -23,4 +23,6 @@ import 'oskari-lazy-loader?routingService!oskari-frontend/packages/framework/bun
 import 'oskari-lazy-loader?maprotator!oskari-frontend/packages/mapping/ol/maprotator/bundle.js';
 import 'oskari-lazy-loader?timeseries!oskari-frontend/packages/framework/bundle/timeseries/bundle.js';
 
+import 'oskari-lazy-loader?metadataflyout!oskari-frontend/packages/catalogue/bundle/metadataflyout/bundle.js';
+
 import './css/overwritten.css';
