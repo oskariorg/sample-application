d2d902017b3db01fc7e091c9b08c6fb479766901
Sami Mäkinen <sami.makinen@nls.fi>
2023-09-04 17:05:03 +0300

    Replace personaldata with mydata

--- applications/geoportal-3d/main.js
+++ applications/geoportal-3d/main.js
@@ -28,7 +28,7 @@ import 'oskari-loader!oskari-frontend/packages/framework/layerlist/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/maplegend/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/myplaces3/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/myplacesimport/bundle.js';
-import 'oskari-loader!oskari-frontend/packages/framework/bundle/personaldata/bundle.js';
+import 'oskari-loader!oskari-frontend/bundles/framework/mydata/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/publisher2/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/statehandler/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/search/bundle.js';
--- applications/geoportal/main.js
+++ applications/geoportal/main.js
@@ -38,6 +38,7 @@ import 'oskari-lazy-loader?featuredata!oskari-frontend/packages/framework/bundle
 import 'oskari-lazy-loader?language-selector!oskari-frontend/packages/framework/bundle/language-selector/bundle.js';
 
 import 'oskari-loader!oskari-frontend/packages/statistics/statsgrid/bundle.js';
+import 'oskari-loader!oskari-frontend/bundles/framework/mydata/bundle.js';
 
 // 3D support
 import 'oskari-loader!oskari-frontend/packages/mapping/olcs/map3dtiles/bundle.js';
@@ -45,8 +46,6 @@ import 'oskari-loader!oskari-frontend/packages/mapping/dimension-change/bundle.j
 
 import 'oskari-loader!../../packages/sample/bundle/sample-info/bundle.js';
 
-import 'oskari-lazy-loader?personaldata!oskari-frontend/packages/framework/bundle/personaldata/bundle.js';
-import 'oskari-lazy-loader?mydata!oskari-frontend/bundles/framework/mydata/bundle.js';
 import 'oskari-lazy-loader?printout!oskari-frontend/packages/framework/bundle/printout/bundle.js';
 import 'oskari-lazy-loader?admin-layereditor!oskari-frontend/packages/admin/bundle/admin-layereditor/bundle.js';
 import 'oskari-lazy-loader?admin-permissions!oskari-frontend/packages/admin/bundle/admin-permissions/bundle.js';
