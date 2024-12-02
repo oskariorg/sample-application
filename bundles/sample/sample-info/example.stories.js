8712dd55045a4956cc847287b5ac3ffcce7da088
Sami Mäkinen <sami.makinen@nls.fi>
2024-12-02 13:57:23 +0200

    Comment out the whole stories.js file

--- bundles/sample/sample-info/example.stories.js
+++ bundles/sample/sample-info/example.stories.js
@@ -1,9 +1,10 @@
+/*
 import React from 'react';
 // https://github.com/oskariorg/oskari-frontend/pull/2662
-//import { storiesOf } from '@storybook/react';
+// import { storiesOf } from '@storybook/react';
 import { Button } from 'oskari-ui';
-
 storiesOf('Sample Application', module)
     .add('with text', () => (
         <Button>Hello from sample application</Button>
     ));
+*/
