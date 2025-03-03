d637f8bb5a466a19c9fb5f14cdaf413c52fb93e6
Sami Mäkinen <sami.makinen@nls.fi>
2025-03-03 13:34:06 +0200

    Migrate loader and paths

--- applications/embedded/main.js
+++ applications/embedded/main.js
@@ -1,4 +1,4 @@
-import 'oskari-loader!oskari-frontend/packages/framework/bundle/mapfull/bundle.js';
+import 'oskari-bundle!oskari-frontend/bundles/framework/mapfull';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/oskariui/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/ui-components/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/divmanazer/bundle.js';
@@ -12,19 +12,20 @@ import 'oskari-loader!oskari-frontend/packages/mapping/ol/infobox/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/toolbar/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/drawtools/bundle.js';
 
-import 'oskari-loader!oskari-frontend/packages/framework/bundle/coordinatetool/bundle.js';
-import 'oskari-loader!oskari-frontend/packages/framework/bundle/publishedstatehandler/bundle.js';
+import 'oskari-bundle!oskari-frontend/bundles/framework/coordinatetool';
+import 'oskari-bundle!oskari-frontend/bundles/framework/publishedstatehandler';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/rpc/bundle.js';
-import 'oskari-loader!oskari-frontend/packages/framework/bundle/maplegend/bundle.js';
+import 'oskari-bundle!oskari-frontend/bundles/framework/maplegend';
 
-import 'oskari-lazy-loader?featuredata!oskari-frontend/packages/framework/featuredata/bundle.js';
-import 'oskari-lazy-loader?statsgrid!oskari-frontend/bundles/statistics/statsgrid/bundle.js';
-import 'oskari-lazy-loader?routingService!oskari-frontend/packages/framework/bundle/routingService/bundle.js';
+import 'oskari-lazy-bundle?featuredata!oskari-frontend/bundles/framework/featuredata';
+import 'oskari-lazy-bundle?statsgrid!oskari-frontend/bundles/statistics/statsgrid';
+import 'oskari-lazy-bundle?routingService!oskari-frontend/bundles/framework/routingService';
 import 'oskari-lazy-loader?maprotator!oskari-frontend/packages/mapping/ol/maprotator/bundle.js';
-import 'oskari-lazy-loader?timeseries!oskari-frontend/packages/framework/bundle/timeseries/bundle.js';
+import 'oskari-lazy-bundle?timeseries!oskari-frontend/bundles/framework/timeseries';
 
-import 'oskari-lazy-loader?metadataflyout!oskari-frontend/bundles/catalogue/metadata/bundle.js'
-import 'oskari-lazy-loader?metadatasearch!oskari-frontend/packages/catalogue/metadatasearch/bundle.js';
-import 'oskari-lazy-loader?announcements!oskari-frontend/packages/framework/bundle/announcements/bundle.js';
+import 'oskari-lazy-bundle?metadataflyout!oskari-frontend/bundles/catalogue/metadataflyout'
+import 'oskari-lazy-bundle?metadatasearch!oskari-frontend/bundles/catalogue/metadatasearch';
+// We don't really need this here IF we don't add announcements for geoportal so they can be added by admins
+// import 'oskari-lazy-bundle?announcements!oskari-frontend/bundles/framework/announcements';
 
 import './css/overwritten.css';
