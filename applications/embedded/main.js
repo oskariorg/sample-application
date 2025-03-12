680ec738feffe35ed0f5409dd13933399dd0e51d
Sami Mäkinen <sami.makinen@nls.fi>
2025-03-12 12:41:44 +0200

    Add comments on bundles, note new/old loaders

--- applications/embedded/main.js
+++ applications/embedded/main.js
@@ -1,31 +1,43 @@
+// main starting point
 import 'oskari-bundle!oskari-frontend/bundles/framework/mapfull';
+
+// jQuery-based UI components
+//  note: old loader
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/oskariui/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/ui-components/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/divmanazer/bundle.js';
 
+// 2D mapmodule and support for additional map layer types
+//  note: old loader
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/mapmodule/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/mapwmts/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/wfsvector/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/mapuserlayers/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/maparcgis/bundle.js';
+
+// additional map related bundles
+//  note: old loader
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/infobox/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/toolbar/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/drawtools/bundle.js';
-
+//  note: new loader
 import 'oskari-bundle!oskari-frontend/bundles/framework/coordinatetool';
 import 'oskari-bundle!oskari-frontend/bundles/framework/publishedstatehandler';
-import 'oskari-loader!oskari-frontend/packages/framework/bundle/rpc/bundle.js';
 import 'oskari-bundle!oskari-frontend/bundles/framework/maplegend';
 
+// RPC connector
+//  note: old loader
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/rpc/bundle.js';
+
+// bundles that are loaded if the user has selected them to be shown on the embedded map
+//  note: old loader
+import 'oskari-lazy-loader?maprotator!oskari-frontend/packages/mapping/ol/maprotator/bundle.js';
+//  note: new loader
 import 'oskari-lazy-bundle?featuredata!oskari-frontend/bundles/framework/featuredata';
 import 'oskari-lazy-bundle?statsgrid!oskari-frontend/bundles/statistics/statsgrid';
 import 'oskari-lazy-bundle?routingService!oskari-frontend/bundles/framework/routingService';
-import 'oskari-lazy-loader?maprotator!oskari-frontend/packages/mapping/ol/maprotator/bundle.js';
 import 'oskari-lazy-bundle?timeseries!oskari-frontend/bundles/framework/timeseries';
-
 import 'oskari-lazy-bundle?metadataflyout!oskari-frontend/bundles/catalogue/metadataflyout'
 import 'oskari-lazy-bundle?metadatasearch!oskari-frontend/bundles/catalogue/metadatasearch';
-// We don't really need this here IF we don't add announcements for geoportal so they can be added by admins
-// import 'oskari-lazy-bundle?announcements!oskari-frontend/bundles/framework/announcements';
 
 import './css/overwritten.css';
--- applications/geoportal/main.js
+++ applications/geoportal/main.js
@@ -1,15 +1,20 @@
+// main starting point
 import 'oskari-bundle!oskari-frontend/bundles/framework/mapfull';
+
+// jQuery-based UI components
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/oskariui/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/ui-components/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/divmanazer/bundle.js';
 
+// 2D mapmodule and support for additional map layer types
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/mapmodule/bundle.js';
-
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/mapwmts/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/maparcgis/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/wfsvector/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/mapmyplaces/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/mapuserlayers/bundle.js';
+
+// additional map related bundles
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/drawtools/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/toolbar/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/infobox/bundle.js';
@@ -17,38 +22,37 @@ import 'oskari-loader!oskari-frontend/packages/mapping/ol/heatmap/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/maprotator/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/userstyle/bundle.js';
 
-import 'oskari-lazy-bundle?metadatasearch!oskari-frontend/bundles/catalogue/metadatasearch';
-import 'oskari-lazy-bundle?metadataflyout!oskari-frontend/bundles/catalogue/metadataflyout'
-
+// other bundles
 import 'oskari-bundle!oskari-frontend/bundles/framework/coordinatetool';
 import 'oskari-bundle!oskari-frontend/bundles/framework/guidedtour';
 import 'oskari-bundle!oskari-frontend/bundles/framework/layerlist';
-
 import 'oskari-bundle!oskari-frontend/bundles/framework/maplegend';
 import 'oskari-bundle!oskari-frontend/bundles/framework/statehandler';
 import 'oskari-bundle!oskari-frontend/bundles/framework/timeseries';
 import 'oskari-bundle!oskari-frontend/bundles/framework/userguide';
-import 'oskari-lazy-bundle?search!oskari-frontend/bundles/framework/search';
-import 'oskari-lazy-bundle?featuredata!oskari-frontend/bundles/framework/featuredata';
-import 'oskari-lazy-bundle?language-selector!oskari-frontend/bundles/framework/language-selector';
 import 'oskari-bundle!oskari-frontend/bundles/framework/findbycoordinates';
 
-// 3D support
+// support for 3D-layer type and 2d/3d switcher
 import 'oskari-loader!oskari-frontend/packages/mapping/olcs/map3dtiles/bundle.js';
 import 'oskari-bundle!oskari-frontend/bundles/mapping/dimension-change';
 
-// app-specific bundle
+// app-specific (example) bundle. Remove from actual app!!
 import 'oskari-bundle!../../bundles/sample-info';
 
-// mobile tuning
+// lazy-loaded as these are not used for every user (allows mobile version etc)
+import 'oskari-lazy-bundle?metadatasearch!oskari-frontend/bundles/catalogue/metadatasearch';
+import 'oskari-lazy-bundle?metadataflyout!oskari-frontend/bundles/catalogue/metadataflyout'
+import 'oskari-lazy-bundle?search!oskari-frontend/bundles/framework/search';
+import 'oskari-lazy-bundle?featuredata!oskari-frontend/bundles/framework/featuredata';
+import 'oskari-lazy-bundle?language-selector!oskari-frontend/bundles/framework/language-selector';
 import 'oskari-lazy-bundle?myplaces3!oskari-frontend/bundles/framework/myplaces3';
 import 'oskari-lazy-bundle?myplacesimport!oskari-frontend/bundles/framework/myplacesimport';
 import 'oskari-lazy-bundle?mydata!oskari-frontend/bundles/framework/mydata';
 import 'oskari-lazy-bundle?publisher2!oskari-frontend/bundles/framework/publisher2';
 import 'oskari-lazy-bundle?statsgrid!oskari-frontend/bundles/statistics/statsgrid';
-// end mobile tuning
-
 import 'oskari-lazy-bundle?printout!oskari-frontend/bundles/framework/printout';
+
+// admin functionalities (lazy-loaded)
 import 'oskari-lazy-bundle?admin-layereditor!oskari-frontend/bundles/admin/admin-layereditor';
 import 'oskari-lazy-bundle?admin-permissions!oskari-frontend/bundles/admin/admin-permissions';
 import 'oskari-lazy-bundle?admin!oskari-frontend/bundles/admin/admin';
